65101416e1357c171e239815482d95cbba597fb1
Jose Casas <31327191+MkWailord@users.noreply.github.com>
2022-06-08 17:19:37 -0500

    Add files via upload

---
+++ and00.vhdl
@@ -0,0 +1,20 @@
+--Primer bloque
+library ieee;
+use ieee.std_logic_1164.all;
+library lattice;
+use lattice.all;
+
+--Segundo bloque entidad
+entity and00 is
+	port(
+		Aa: in std_logic_vector(7 downto 0);
+		Ba: in std_logic_vector(7 downto 0);
+		Ya: out std_logic_vector(7 downto 0)
+	);
+end and00;
+
+--Tercer bloque arquitectura
+architecture and0 of and00 is
+begin
+	Ya <= Aa and Ba;
+end and0;
